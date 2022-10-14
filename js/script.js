7dbf73fa124bdc7583a7df5b2f5322d9fd389374
mirmammad.gafarov@gmail.com <mirmammad.gafarov@gmail.com>
2022-10-14 22:32:09 +0400

    initial version

---
+++ js/script.js
@@ -0,0 +1,33 @@
+document.getElementById("left").innerText = "<Header";
+document.getElementById("middle").innerText = "{M:Gafarov} ";
+document.getElementById("right").innerText = " />";
+
+
+
+
+// Fixed Navigation Bar all over the page
+$(window).scroll(() => {
+  var scroll = $(window).scrollTop();
+
+  if (scroll >= 300) {
+    $(".sc-media").addClass("make-fixed");
+  } else {
+    $(".sc-media").removeClass("make-fixed");
+  }
+});
+
+const projects = ["construction", "canvas", "meladora"];
+projects.forEach((project) => {
+  $("#" + project).on({
+    mouseover: function () {
+      $("#" + project + " .overlay").css("opacity", "0.8");
+      $("#" + project + " .overlay").css("animation", "show 1s");
+      $("#" + project + ".work img").css("transform", "scale(1.1)");
+    },
+    mouseout: function () {
+      $("#" + project + " .overlay").css("opacity", "0");
+      $("#" + project + " .overlay").css("animation", "hide 1s");
+      $("#" + project + ".work img").css("transform", "scale(1)");
+    },
+  });
+});
